d08d8c6f480c4f8aa3f3f6d05069149ce6970d3d
Penjami <penjami.rantakangas@cs.tamk.fi>
2017-11-17 13:47:21 +0200

    Documention for GameObject class.

--- src/fi/tamk/tikoot/GameObject.java
+++ src/fi/tamk/tikoot/GameObject.java
@@ -7,7 +7,7 @@ import javafx.scene.image.Image;
 /**
  * Created by Penjami on 5.11.2017.
  */
-public class GameObject
+public abstract class GameObject
 {
 
     private double positionX;
@@ -16,44 +16,82 @@ public class GameObject
     private double velocityX;
 
 
-    public GameObject()
-    {
-        positionX = 0;
-        positionY = 0;
-    }
-
-
+    /**
+     * Sets the position of the object'
+     *
+     * @param x Position in the x axis.
+     * @param y Position in the y axis.
+     */
     public void setPosition(double x, double y)
     {
         positionX = x;
         positionY = y;
     }
+
+    /**
+     * Updates the gameobjects position based on velocity.
+     *
+     * @param time The time between frames.
+     */
     public void update(double time)
     {
         setPosition(getPositionX() + velocityX * time, getPositionY() + velocityY * time );
     }
 
+    /**
+     * Get the y axis velocity of the object.
+     *
+     * @return Y axis velocity.
+     */
     public double getVelocityY() {
         return velocityY;
     }
 
+    /**
+     * Sets the y axis velocity of the object.
+     *
+     * @param velocityY Y axis velocity.
+     */
     public void setVelocityY(double velocityY) {
         this.velocityY = velocityY;
     }
 
+    /**
+     * Get the x axis velocity of the object.
+     *
+     * @return X axis velocity.
+     */
     public double getVelocityX() {
         return velocityX;
     }
 
+    /**
+     * Sets the x axis velocity of the object.
+     *
+     * @param velocityX X axis velocity.
+     */
     public void setVelocityX(double velocityX) {
         this.velocityX = velocityX;
     }
+
+    /**
+     * Sets the velocity of both axis of the object.
+     *
+     * @param x X axis velocity.
+     * @param y Y axis velocity.
+     */
     public void setVelocity(double x, double y)
     {
         setVelocityX(x);
         setVelocityY(y);
     }
 
+    /**
+     * Adds to the current velocity of both axis of the object.
+     *
+     * @param x X axis velocity.
+     * @param y Y axis velocity.
+     */
     public void addVelocity(double x, double y)
     {
         velocityX += x;
@@ -61,11 +99,16 @@ public class GameObject
     }
 
 
-
+    /**
+     * @return The x position of the object.
+     */
     public double getPositionX() {
         return positionX;
     }
 
+    /**
+     * @return The y position of the object.
+     */
     public double getPositionY() {
         return positionY;
     }
