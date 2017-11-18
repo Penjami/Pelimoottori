08d704b724d44b69238b7d925f03c810e1f4ef81
Penjami <penjami.rantakangas@cs.tamk.fi>
2017-11-18 12:53:32 +0200

    Documentation for Sprite and TextObject classes

--- src/fi/tamk/tikoot/pelimoottori/Sprite.java
+++ src/fi/tamk/tikoot/pelimoottori/Sprite.java
@@ -13,48 +13,94 @@ public class Sprite extends GameObject {
     private double width;
     private double height;
 
+    /**
+     * Constructor for sprite object.
+     */
     public Sprite() {}
+
+    /**
+     * Constructor for sprite object.
+     *
+     * @param fileLocation Location of the sprite.
+     */
     public Sprite(String fileLocation) {
         setImage(fileLocation);
     }
 
-    void setImage(Image i)
+    /**
+     * Sets the sprite objects image.
+     *
+     * @param i The image.
+     */
+    public void setImage(Image i)
     {
         image = i;
         width = i.getWidth();
         height = i.getHeight();
     }
 
+    /**
+     * Sets the sprite objects image based on the filename.
+     *
+     * @param filename The filename of the image.
+     */
     public void setImage(String filename)
     {
         Image i = new Image(filename);
         setImage(i);
     }
 
+    /**
+     * Used to render the sprite object.
+     *
+     * @param gc GraphicsContext object that is used to draw sprites.
+     */
     public void render(GraphicsContext gc)
     {
         gc.drawImage( image, getPositionX(),getPositionY() );
     }
 
+    /**
+     * Used to get the boundaries of this object.
+     */
     public Rectangle2D getBoundary()
     {
         return new Rectangle2D(getPositionX(),getPositionY(),getWidth(),getHeight());
     }
 
+    /**
+     * Used to check if this object intersects with a certain other sprite object.
+     *
+     * @param g the other sprite object.
+     */
     public boolean intersects(Sprite g)
     {
         return g.getBoundary().intersects( this.getBoundary() );
     }
 
-
+    /**
+     * Used to get the image of this sprite object.
+     *
+     * @return The image of this sprite object.
+     */
     public Image getImage() {
         return image;
     }
 
+    /**
+     * Used to get the width of this sprite object.
+     *
+     * @return the width of this sprite object.
+     */
     public double getWidth() {
         return width;
     }
 
+    /**
+     * Used to get the height of this sprite object.
+     *
+     * @return the height of this sprite object.
+     */
     public double getHeight() {
         return height;
     }
--- src/fi/tamk/tikoot/pelimoottori/TextObject.java
+++ src/fi/tamk/tikoot/pelimoottori/TextObject.java
@@ -13,29 +13,61 @@ public class TextObject extends GameObject {
     private String text;
     private Color textColor;
 
+    /**
+     * Constructor for text object.
+     *
+     * @param text The text that is being shown.
+     * @param textColor The color of the text.
+     * @param x Position x of the text object.
+     * @param y Position y of the text object.
+     */
     public TextObject(String text, Color textColor, double x, double y) {
         this.text = text;
         this.textColor = textColor;
         setPosition(x,y);
     }
 
+    /**
+     * Constructor for text object.
+     *
+     * @param text The text that is being shown.
+     */
     public void setText(String text) {
         this.text = text;
     }
 
+    /**
+     * Used to get the text to of this object.
+     *
+     * @return The text that is being shown.
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Used to get the text color of this object.
+     *
+     * @return The text color.
+     */
     public Color getTextColor() {
         return textColor;
     }
 
+    /**
+     * Used to set the text color of this object.
+     *
+     * @param textColor The text color.
+     */
     public void setTextColor(Color textColor) {
         this.textColor = textColor;
     }
 
-
+    /**
+     * Used to render this text object.
+     *
+     * @param gc GraphicsContext object that is used to draw text objects.
+     */
     public void render(GraphicsContext gc)
     {
         gc.setFill(textColor);
