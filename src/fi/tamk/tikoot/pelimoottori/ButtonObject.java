13a919e3837947dd62b67071e6be1f996993f6db
Penjami <penjami.rantakangas@cs.tamk.fi>
2017-11-23 10:29:41 +0200

    - ButtonObject class added methods and constructor.

--- src/fi/tamk/tikoot/pelimoottori/ButtonObject.java
+++ src/fi/tamk/tikoot/pelimoottori/ButtonObject.java
@@ -1,4 +1,21 @@
 package fi.tamk.tikoot.pelimoottori;
 
-public class ButtonObject {
+import javafx.scene.control.Button;
+
+public class ButtonObject extends GameObject {
+
+    private Button button;
+
+    public ButtonObject(String text) {
+        super();
+        button = new Button(text);
+    }
+
+    public Button getButton() {
+        return button;
+    }
+
+    public void setButton(String text) {
+        this.button = new Button(text);
+    }
 }
